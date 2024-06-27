8a106042afddb63c98dc110960b6b5b06d3cbd9e
MattyHD0 <MattyHD0@hotmail.com>
2024-06-27 17:25:04 -0300

    removed unused config updater

--- src/main/java/me/mattyhd0/chatcolor/configuration/ConfigurationManager.java
+++ src/main/java/me/mattyhd0/chatcolor/configuration/ConfigurationManager.java
@@ -13,23 +13,6 @@ public class ConfigurationManager {
         this.messages = new MessagesYMLFile("messages.yml");
         this.patterns = new SimpleYMLConfiguration("patterns.yml");
         this.data = new SimpleYMLConfiguration("playerdata.yml");
-        /*if(config.getBoolean("config.auto-update-config",true)) {
-            updateConfig();
-            updateMessages();
-        }*/
-    }
-
-    private void updateMessages() {
-        boolean updated = addConfig(messages,"messages.commands.chatcolor.player-not-loaded","%prefix% &cReconnect to the server. If issue persist, contact an administrator.");
-        updated = addConfig(messages,"messages.commands.chatcoloradmin.player-not-loaded","%prefix% &cTarget player is not loaded. Try in some seconds..") || updated;
-        if(updated) messages.save();
-    }
-
-    private void updateConfig() {
-        boolean updated = addConfig(config,"config.mysql.additional-url","&useSSL=false&autoReconnect=true");
-        updated = addConfig(config,"config.data-delay",30) || updated;
-        updated = addConfig(config,"config.auto-update-config",true) || updated;
-        if(updated) config.save();
     }
 
     public SimpleYMLConfiguration getConfig() {
@@ -52,11 +35,4 @@ public class ConfigurationManager {
         return data;
     }
 
-    public boolean addConfig(SimpleYMLConfiguration config, String path, Object value) {
-        if(!config.contains(path)) {
-            config.set(path,value);
-            return true;
-        }
-        return false;
-    }
 }
